9efc334752e088bf71ad57a76804c7676db95963
hanqyu <peakjnut37@gmail.com>
2020-09-19 16:36:23 +0900

    build(pip): settings completed for development env

--- django_docker/settings/dev.py
+++ django_docker/settings/dev.py
@@ -3,6 +3,8 @@ from .base import *  # noqa
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, 'config/envs/.env.dev'))
 
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
