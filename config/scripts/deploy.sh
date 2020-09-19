8685f6f97809bce049c1417528d73548e0ffe62b
hanqyu <peakjnut37@gmail.com>
2020-09-20 00:32:09 +0900

    fix(script): docker, docker-compose 설치 스크립트 추가

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-# Installing docker engine
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# Installing docker engine if not exists
+if ! docker -v COMMAND &> /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install -y docker-ce
+fi
+
+# Installing docker-compose if not exists
+if ! docker-compose -v COMMAND &> /dev/null
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 cd ~/srv/django-docker/ && docker-compose -f docker-compose.prod.yml up --build -d
